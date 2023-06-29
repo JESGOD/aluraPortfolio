aef02564fcedff4766a2af2a1859aab4ec4e3de6
JESGOD <yonnyes@unicauca.edu.co>
2023-06-29 13:20:27 -0500

    se agregaron todas las validaciones solo falta que al enviar se borre el contenido de los campos

--- js/validaciones.js
+++ js/validaciones.js
@@ -7,10 +7,16 @@ formulario.addEventListener("submit",e =>{
     let warnings = "";
     let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
     if(inputs[0].value.length >= 50){
-    
         alert("el nombre debe contener máximo 50 caracteres")
     }
     if(!regexEmail.test(inputs[1].value)){
         alert("el email no es valido")
     }
+    if(inputs[2].value.length >= 50){
+        alert("el asunto debe contener máximo 50 caracteres")
+    }
+    if(inputs[3].value.length >= 300){
+        alert("el mensaje debe contener máximo 300 caracteres")
+    }
+    
 })
